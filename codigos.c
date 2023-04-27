5ef2132dfb1045705729b6abb563d361fa6e0771
jonathan-castraneda <cf20014@ues.edu.sv>
2023-04-27 08:23:17 -0600

    update:Se logro el codigo para recortar lineas

--- codigos.c
+++ codigos.c
@@ -15,6 +15,7 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <GL/freeglut.h>
+#include <math.h>
 
 void codigoLinea() {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -34,9 +35,72 @@ void codigoCirculo() {
 
 void codigoCohenSutherland() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0, 0, 1);
-    glRasterPos2d(0.5, 0.5);
-    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_10, "Codigo de Cohen Sutherland");
+    //Variables de los puntos de la línea
+    float x1 = 5, y1 = 600, x2 = 550, y2 = 5;
+    int minw = 200, maxw = 400;
+    float pendiente = 0;
+    //Genera las lineas para visualizar los nueve campos
+    glColor3f(0, 0, 0);
+    glBegin(GL_LINES);
+    for (int i = 200; i < 600; i += 200) {
+        glVertex2f(i, 0);
+        glVertex2f(i, 600);
+        glVertex2f(0, i);
+        glVertex2f(600, i);
+    }
+    glEnd();
+    //CODIGO para generar la linea
+    pendiente = (y2 - y1) / (x2 - x1);
+    //PARA X1 Y Y1
+    //Arriba
+    if (y1 > maxw) {
+        x1 = (maxw + (pendiente * x1) - y1) / pendiente;
+        y1 = maxw;
+    }
+    //Abajo
+    if (y1 < minw) {
+        x1 = (minw + (pendiente * x1) - y1) / pendiente;
+        y1 = minw;
+    }
+    //Izquierda
+    if (x1 < minw) {
+        y1 = (pendiente * (minw - x1)) + y1;
+        x1 = minw;
+    }
+    //Derecha
+    if (x1 > maxw) {
+        y1 = (pendiente * (maxw - x1)) + y1;
+        x1 = maxw;
+    }
+    //PARA X2 Y Y2
+    //Arriba
+    if (y2 > maxw) {
+        x2 = (maxw + (pendiente * x2) - y2) / pendiente;
+        y2 = maxw;
+    }
+    //Abajo
+    if (y2 < minw) {
+        x2 = (minw + (pendiente * x2) - y2) / pendiente;
+        y2 = minw;
+    }
+    //Izquierda
+    if (x2 < minw) {
+        y2 = (pendiente * (minw - x2)) + y2;
+        x2 = minw;
+    }
+    //Derecha
+    if (x2 > maxw) {
+        y2 = (pendiente * (maxw - x2)) + y2;
+        x2 = maxw;
+    }
+    
+    
+    glColor3f(1, 0, 0);
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+    glEnd();
+
     glFlush();
 }
 
--- main.c
+++ main.c
@@ -14,9 +14,10 @@
 #include "codigos.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <GL/gl.h>
-#include <GL/glut.h>
-#include <GL/glu.h>
+#include<GL/glut.h>
+#include<GL/glu.h>
+#include<GL/gl.h>
+#include<GL/freeglut.h>
 
 /*
  * 
@@ -24,11 +25,13 @@
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowPosition(50,50);
     glutInitWindowSize(600, 600);
     glutCreateWindow("Codigos");
-    glClearColor(0.8,0,0,1);
-    glutDisplayFunc(codigoSutherlandHodgeman);
+    glClearColor(1,1,1, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    gluOrtho2D(0,600,0,600);
+    glutDisplayFunc(codigoCohenSutherland);
     glutMainLoop();
     
     
